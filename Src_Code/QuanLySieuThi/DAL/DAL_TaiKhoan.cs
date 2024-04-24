0ac016075d26b37625920aa7c4fe45c8c636f0e8
NhatTaiC <nhattai0501@gmail.com>
2024-04-25 02:47:28 +0700

    Created DAL_TaiKhoan.cs

---
+++ Src_Code/QuanLySieuThi/DAL/DAL_TaiKhoan.cs
@@ -0,0 +1,228 @@
+﻿/*
+ * Châu Nhật Tài, Lê Văn Toàn
+ * Project CN.NET
+ * Quản Lý Siêu Thị
+ * 22/04/2024
+ * DAL_TaiKhoan.cs
+ */
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAL
+{
+    public class DAL_TaiKhoan
+    {
+        // Fields
+        private QLSTDataContext db;
+        private string serverName = ".\\SQLEXPRESS";
+        private string dbName = "QLST";
+
+        // Constructors
+        public DAL_TaiKhoan(QLSTDataContext db, string serverName, string dbName)
+        {
+            this.db = db;
+            this.serverName = serverName;
+            this.dbName = dbName;
+        }
+
+        public DAL_TaiKhoan()
+        {
+            string sql = "Data Source=" + ServerName + ";Initial Catalog=" + DbName + ";Integrated Security=True;";
+            Db = new QLSTDataContext(sql);
+        }
+
+        // Properties
+        public QLSTDataContext Db { get => db; set => db = value; }
+        public string ServerName { get => serverName; set => serverName = value; }
+        public string DbName { get => dbName; set => dbName = value; }
+
+        // Methods
+        // LayDSTK()
+        public IQueryable LayDSTK()
+        {
+            // Lấy DSTK
+            IQueryable temp = from t in db.TaiKhoans
+                              select new
+                              {
+                                  TaiKhoan = t.TaiKhoan1,
+                                  MatKhau = t.MatKhau,
+                                  HoTen = t.HoTen,
+                                  NgayTao = t.NgayTao,
+                                  ChucVu = t.ChucVu
+                              };
+            return temp;
+        }
+
+        // ThemTK()
+        public bool ThemTK(DTO_TaiKhoan tk)
+        {
+            try
+            {
+                // Tạo đối tượng TaiKhoan
+                TaiKhoan t_insert = new TaiKhoan
+                {
+                    TaiKhoan1 = tk.TaiKhoan,
+                    MatKhau = tk.MatKhau,
+                    HoTen = tk.HoTen,
+                    NgayTao = DateTime.Parse(tk.NgayTao),
+                    ChucVu = tk.ChucVu
+                };
+
+                // Check xem đã có TaiKhoan trong DB TaiKhoan chưa?
+                var temp = from t in db.TaiKhoans
+                           where t.TaiKhoan1 == tk.TaiKhoan
+                           select t;
+
+                if (temp.Count() != 1)
+                {
+                    // Check TaiKhoan có # null ko mới add vào DB TaiKhoan
+                    if (t_insert.TaiKhoan1 != string.Empty)
+                    {
+                        db.TaiKhoans.InsertOnSubmit(t_insert); // Thêm DB TaiKhoan
+                        db.SubmitChanges(); // Xác nhận thay đổi DB TaiKhoan
+
+                        // Thông báo
+                        MessageBox.Show("Thêm thành công!", "Thông báo",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
+                        return true;
+                    }
+                    else
+                    {
+                        // Thông báo
+                        MessageBox.Show("Tài khoản không hợp lệ, không thể thêm TaiKhoan mới!", "Thông báo",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    // Thông báo
+                    MessageBox.Show("Đã có TaiKhoan trong DB TaiKhoan!", "Thông báo",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Throw Exception
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+
+        // XoaTK()
+        public bool XoaTK(string tk)
+        {
+            try
+            {
+                // Check TaiKhoan có # null ko? mới xóa
+                if (tk != string.Empty)
+                {
+                    // Truy vấn NhanVien có cùng TaiKhoan và xóa đi trước khi thông báo
+                    var tk_nv = from t in db.NhanViens
+                                where t.MaNV == tk
+                                select t;
+
+                    // Xóa NhanVien có cùng TaiKhoan
+                    foreach (var item in tk_nv)
+                    {
+                        db.NhanViens.DeleteOnSubmit(item); // Xóa NhanVien có cùng TaiKhoan trong DB NhanVien
+                        db.SubmitChanges(); // Xác nhận thay đổi DB NhanVien
+                    }
+
+                    // Truy vấn TaiKhoan có trong DSTK hay ko?
+                    var t_delete = from t in db.TaiKhoans
+                                   where t.TaiKhoan1 == tk
+                                   select t;
+
+                    // Xóa TK
+                    foreach (var item in t_delete)
+                    {
+                        db.TaiKhoans.DeleteOnSubmit(item); // Xóa TaiKhoan trong DB TaiKhoan
+                        db.SubmitChanges(); // Xác nhận thay đổi DB TaiKhoan
+                    }
+
+                    // Thông báo
+                    MessageBox.Show("Xóa thành công!", "Thông báo",
+                       MessageBoxButtons.OK,
+                       MessageBoxIcon.Information);
+                    return true;
+                }
+                else
+                {
+                    // Thông báo
+                    MessageBox.Show("Tài khoản không hợp lệ, không thể xóa TaiKhoan!", "Thông báo",
+                       MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Throw Exception
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+
+        // SuaTK()
+        public bool SuaTK(DTO_TaiKhoan tk)
+        {
+            try
+            {
+                // Tạo đối tượng TaiKhoan = cách truy vấn DB
+                TaiKhoan t_update = db.TaiKhoans.Single(t => t.TaiKhoan1 == tk.TaiKhoan);
+
+                // Check TaiKhoan != Null ko? mới sửa thông tin TaiKhoan
+                if (t_update.TaiKhoan1 != string.Empty)
+                {
+                    // Sửa thông tin TaiKhoan
+                    t_update.MatKhau = tk.MatKhau;
+                    t_update.HoTen = tk.HoTen;
+                    t_update.NgayTao = DateTime.Parse(tk.NgayTao);
+                    t_update.ChucVu = tk.ChucVu;
+
+                    // Xác nhận thay đổi DB
+                    db.SubmitChanges();
+
+                    // Thông báo
+                    MessageBox.Show("Sửa thành công!", "Thông báo",
+                       MessageBoxButtons.OK,
+                       MessageBoxIcon.Information);
+                    return true;
+                }
+                else
+                {
+                    // Thông báo
+                    MessageBox.Show("Tài khoản không hợp lệ, không thể sửa thông tin TaiKhoan!", "Thông báo",
+                       MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Throw Exception
+                MessageBox.Show(ex.Message);
+            }
+            return false;
+        }
+
+        // LayDSTK_TheoTK()
+        public IQueryable LayDSTK_TheoTK(string tk)
+        {
+            // Lấy DSTK Theo TK
+            IQueryable temp = from t in db.TaiKhoans
+                              where t.TaiKhoan1 == tk
+                              select new
+                              {
+                                  TaiKhoan = t.TaiKhoan1,
+                              };
+            return temp;
+        }
+    }
+}
