0818c0b20996d89a1e556e4ac55a3e60f65fadca
NhatTaiC <nhattai0501@gmail.com>
2024-05-11 01:47:59 +0700

    Report 1

--- Src_Code/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
+++ Src_Code/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace QuanLySieuThi.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KOH-LAPTOP\\SQLEXPRESS;Initial Catalog=QLST;Integrated Security=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=QLST;Integrated Security=True;")]
         public string tspConnect {
             get {
                 return ((string)(this["tspConnect"]));
@@ -36,8 +36,8 @@ namespace QuanLySieuThi.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KOH-LAPTOP\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\QLST.mdf;Integ" +
-            "rated Security=True;User Instance=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\QLST.mdf;Integrated Sec" +
+            "urity=True;User Instance=True")]
         public string mdfConnect {
             get {
                 return ((string)(this["mdfConnect"]));
