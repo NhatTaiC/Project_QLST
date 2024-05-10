2411a14c2ef1dd917ab95a4dcda975b5a72cf7a5
NhatTaiC <nhattai0501@gmail.com>
2024-05-11 00:48:59 +0700

    Final 1

--- Src_Code/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
+++ Src_Code/QuanLySieuThi/QuanLySieuThi/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace QuanLySieuThi.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=QLST;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=KOH-LAPTOP\\SQLEXPRESS;Initial Catalog=QLST;Integrated Security=True;T" +
+            "rustServerCertificate=True")]
         public string tspConnect {
             get {
                 return ((string)(this["tspConnect"]));
---
+++ Src_Code/Text/ReadMe.txt
@@ -0,0 +1,13 @@
+/*
+Ứng dụng quản lý siêu thị
+Phiên bản: 1.0
+Tạo bởi: Nhật Tài, Văn Toàn
+*/
+
++++++ Hướng Dẫn +++++
+
+1. Vui lòng chạy file create_qlst_data.bat trong thư mục Data trước!
+2. Sửa lại serverName khi chạy ứng dụng bị lỗi connection.
+
+Note: Nếu chạy file create_qlst_data.bat bị lỗi, vui lòng Attach thủ công 2 file QLST.mdf và QLST_log.mdf trong thư mục
+Data vào SSMS
